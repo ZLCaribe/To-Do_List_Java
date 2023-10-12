76a5253e6a218f6e07596a383809ac2c03863631
José Luiz <ZLAmoedo@gmail.com>
2023-10-12 23:03:21 +0100

    Second and Third Classes

--- todolist/src/main/java/com/zl/todolist/user/UserController.java
+++ todolist/src/main/java/com/zl/todolist/user/UserController.java
@@ -1,9 +1,14 @@
 package com.zl.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 /**
  * Class Modifiers:
@@ -15,20 +20,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
     
-    /**
-     * String (text)
-     * Integer (int) integer numbers
-     * Float (float) floating point numbers e.g 0.0001
-     * Double (double) double floating point numbers e.g 0.000001
-     * Character (char) e.g A, B, C
-     * Date
-     * void
-     */
-    /**
-     * Body
-     */
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.name);
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null){
+            //Mensagem de erro
+            //Status code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashed);
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
